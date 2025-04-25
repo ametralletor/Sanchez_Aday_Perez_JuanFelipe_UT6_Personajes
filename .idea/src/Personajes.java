f8c07ee40cf9d408e0801c6e629dfa84e544b794
ametralletor <adaysg14@gmail.com>
2025-04-25 20:13:44 +0100

    Añadidos los toString, equals, hashCode

--- .idea/src/Personajes.java
+++ .idea/src/Personajes.java
@@ -1,13 +1,35 @@
 abstract class Personajes {
+    int id;
     String nombre;
     int nivel;
     int salud;
 
-    public Personajes(String nombre, int nivel, int salud) {
+    public Personajes(int id, String nombre, int nivel, int salud) {
+        this.id = id;
         this.nombre = nombre;
         this.nivel = nivel;
         this.salud = salud;
     }
 
     abstract void atacar();
+
+    @Override
+    public String toString() {
+        return "Personaje: " + nombre + ", Nivel: " + nivel + ", Salud: " + salud;
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if (this == obj) return true;
+        if(obj==null || getClass() != obj.getClass()) return false;
+        Personajes other = (Personajes) obj;
+        return id==other.id &&nombre.equals(other.nombre) &&nivel == other.nivel &&salud==other.salud;
+    }
+
+
+    //MIRAR ESTO Y HACER HASHCODE PARA EL ID Y QUE UN ID NO SE REPITA
+    @Override
+    public int hashCode(){
+        return nombre.hashCode();
+    }
 }
